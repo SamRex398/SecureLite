234179160c8dd27e6b5dc583689751f298e3b94e
SamRex398 <136026805+SamRex398@users.noreply.github.com>
2026-04-29 09:43:59 +0100

    cors error fix 2

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://securelite-2.onrender.com",
+  origin: (process.env.CORS_ORIGINS || "http://localhost:5173"||"https://securelite-2.onrender.com"),
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
